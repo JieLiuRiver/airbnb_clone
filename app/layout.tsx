4955dc44a6c072c151b5b5f629309f67c2aea4ed
Bode He <bode_he@epam.com>
2023-06-09 16:52:21 +0800

    feat: add ClientOnly component to ensure client component work well

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,28 @@
-import Navbar from './components/Navbar'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import ClientOnly from "./components/ClientOnly";
+import Navbar from "./components/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Aribnb',
-  description: 'Airbnb clone',
-}
+  title: "Aribnb",
+  description: "Airbnb clone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}> <Navbar /> {children}</body>
+      <body className={inter.className}>
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
